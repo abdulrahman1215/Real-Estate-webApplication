a1fa0ad4c190059213e3039dfcf6c303df7db462
MD ABDULRAHMAN <mdabdulrahmanaslam@gmail.com>
2025-06-14 11:28:53 +0530

    create api

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+  res.send('✅ Test route working!');
+};
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -21,3 +22,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port 3000`);
 });
+
+app.use('/api/user', userRouter);
