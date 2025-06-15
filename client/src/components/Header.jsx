f75cac44268d36baf2be78c4183da6a29eacfab1
MD ABDULRAHMAN <mdabdulrahmanaslam@gmail.com>
2025-06-15 19:16:21 +0530

    update header and privateRoute

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,13 +13,13 @@ export default function Header() {
             <span className='text-slate-1000'>Estate</span>
         </h1>
         </Link>
-        <from className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input 
             type="text" 
             placeholder="Search..." 
             className='bg-transparent focus:outline-none w-24 sm:w-64' />
             <FaSearch className='text-slate-600'/>
-        </from>
+        </form>
         <ul className='flex gap-4'>
             <Link to ='/home'>
             <li className='hidden sm:inline text-slate-600 hover:underline'>Home</li>
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Outlet, Navigate } from 'react-router-dom'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute() {
-    const { user } = useSelector((state) => state.user)
-  return currentUser ? <Outlet/> : <Navigate to='/sign-in'/>;
+  const currentUser = useSelector((state) => state.user.currentUser);
+
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
