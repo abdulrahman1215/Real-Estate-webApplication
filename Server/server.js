a3445e16c1125b4c5951ec19e3a10fb3aa16cda9
MD ABDULRAHMAN <mdabdulrahmanaslam@gmail.com>
2025-06-16 10:13:28 +0530

    create user update api

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser  from 'cookie-parser';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(express.json());
 const PORT = 3000;
+
+app.use(cookieParser());
 
 
 
